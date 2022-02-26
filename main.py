fb1b6b88ac9ddf28ee4f148224cfabbd37cc114f
Cfrazilus1 <99999698+Cfrazilus1@users.noreply.github.com>
2022-02-26 00:55:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+user_text = input('Enter some text: ')
+
+upper_or_lower = input("Enter 1 to uppercase and 2 to lowercase: ")
+
+if upper_or_lower == "1":
+  print(user_text.upper())
+else:
+  print(user_text.lower())
+
+
+#user_number = input('What do you want to double? ')
+
+#print(int(user_number) * 2)
